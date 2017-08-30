b4930aa423268b1fe8cef66c1da76364e443f099
KingCosmic <kingcosmic2016@gmail.com>
2017-08-30 15:32:35 -0500

    changed config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
   line: {
-    channelAccessToken: "bGaGlmUWdnJgud8qzM5IK6jdli7z3V0rzXTqMovx1VvpRY8FTzwLQq7PH28lmLoECQWRElGsuddmH+NA9vxkW2x/NvJ+Ish6M1F/tJsgapPDw8a7uYev/5RmJ7CK+Vw7cP2/BokkYv7PWRE5wdYu/QdB04t89/1O/w1cDnyilFU=",
-    channelSecret: "2d4703f4b70dfbddfd3dd7324c3b5061",
+    channelAccessToken: "",
+    channelSecret: "",
     port: "8080"
   },
   updateInterval: 60 * 10, // 10 Minutes
-  cocApiKey: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjFiZmQyODQ1LWJkOTktNDE4Ni05NmJlLWU1ZjNjNGM1YjgwZCIsImlhdCI6MTUwNDA3MTQ3MSwic3ViIjoiZGV2ZWxvcGVyLzcxYjUxNTAxLWYwZGYtNDUxZi1mZjcyLTFiZGMzMTI5ODYwMSIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjc1LjEyMC4yMzcuNDYiXSwidHlwZSI6ImNsaWVudCJ9XX0.2PmLAu3czFtjgfTYsWQ-bs8tSxnBt5Z1JlsNZ7NcSHQml3GNP5j7BTu1fQnB6yx-8l_W0Bpie_BS7eksIo_QSQ",
-  clanTag: "#8YY2LP0U",
+  cocApiKey: "",
+  clanTag: "",
   finalMinutes: 15,
   messages: {
     prepDay: {

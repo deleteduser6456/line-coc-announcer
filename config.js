f1c7a6d3542ae653c12a13f99aad1aeb7d2c21bf
deleteduser6456 <31812080+deleteduser6456@users.noreply.github.com>
2017-09-11 01:35:10 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
   line: {
-    channelAccessToken: "iNU2o9V2Tx8+Z5kOVFHDjaR0ORI1mN4O3VZsdjmMv4hzxiFyaoW5oYVkFNTCps0qQpJjPtitnMca+UPMu5KGavyM+8+vYOvjjRvXxVSo8LTYYwnOS0tiDcYCeIbc6R9U6kkQdB04t89/1O/w1cDnyilFU=",
-    channelSecret: "2275fd919dff9cca8cf0989",
+    channelAccessToken: "",
+    channelSecret: '',
     port: "8080"
   },
   updateInterval: 60 * 3, // 10 Minutes
-  cocApiKey: "eyJ0eXAiOiJKV1QiLCJhI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjczNThjY2U0LTZjNzQtNDNkOC04NzQ3LWU5Y2NjZjQ1NzkyNCIsImlhdCI6MTUwNDUzNTYxMiwic3ViIjoiZGV2ZWxvcGVyLzIxMzY3ZWE1LTE0NDctZTk0Zi0zYzExLTFmOWY4YzViMmU5YyIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjEwMy4zLjE0Ni4xNzYiXSwidHlwZSI6ImNsaWVudCJ9XX0.q8ZpDkZAZJCSTjZRoPvZ9oxDkQ7R7fTftVS3SoN-aExJ1tdRikQ-85JmawnxdAwJ9HklUE1z6Rh5wk_O3Y6piw",
-  clanTag: "#8YY2LP0U",
+  cocApiKey: ''
+  clanTag: '',
   finalMinutes: 15,
   messages: {
     prepDay: {
